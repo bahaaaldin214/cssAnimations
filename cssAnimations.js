56ebe6ac66e7e6c703202581cf8ec3ad37bc51fc
Bahaa <bahaaaldinmohammad@gmail.com>
2019-07-11 03:21:52 -0500

    Update cssAnimations.js

--- cssAnimations.js
+++ cssAnimations.js
@@ -1,4 +1,12 @@
 const css = {};
+Array.isNodeList = function (nodes) {
+    var stringRepr = Object.prototype.toString.call(nodes);
+
+    return typeof nodes === 'object' &&
+        /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
+        (typeof nodes.length === 'number') &&
+        (nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
+};
 class cssFunction {
     constructor(toChanges, changes, time) {
         this.toChanges = toChanges;
